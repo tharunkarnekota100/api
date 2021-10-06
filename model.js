0996cb427279af6d2ba0090cfc0d804d356d9743
tharunkarnekota <Tharunkarnekota@gmail.com>
2021-10-06 10:45:54 +0530

    api

---
+++ model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const BrandName = mongoose.Schema({
+    brandname : {
+        type : String,
+        required: true,
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    }
+
+})
+
+module.exports = mongoose.model('brandname',BrandName)
